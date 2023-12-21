b599ac0101bd7fadf444a2e7535429e056b20cad
filippo cafaro <144951463+FilippoDibe@users.noreply.github.com>
2023-12-21 16:16:37 +0100

    add img end config appheader

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    name: 'AppHeader',
+}
+
+
+</script>
+
+<template>
+    <header class="header">
+        <div class="logo">
+            <img :src="" alt="">
+        </div>
+    </header>
+</template>
+
+<style lang="scss" scoped></style>
